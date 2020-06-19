aab27523f2c769a271d5b4fbee45e81f9dc7d147
aselyacan <67157017+aselyacan@users.noreply.github.com>
2020-06-19 18:16:48 +0200

    Update TestClass.java

--- src/TestClass.java
+++ src/TestClass.java
@@ -1,5 +1,6 @@
 public class TestClass {
     public static void main(String[] args) {
         System.out.println("Git den Merhabalar !");
+          System.out.println("GitHub dedi iskender aselyacan  den Merhabalar !");
     }
 }
